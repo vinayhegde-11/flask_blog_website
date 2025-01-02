856c1ebe218e7bd26235e79c2382a49eeb118cd4
vinayhegde-11 <itsvinayhegde@gmail.com>
2025-01-02 09:07:55 +0530

    html rendering using templates and html inheritance using layout.html

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Vinay Hegde',
+        'title': 'My First Blog',
+        'content': 'Some Content',
+        'date_posted': 'January 02, 2025'
+    },
+    {
+        'author': 'test',
+        'title': 'test blog',
+        'content': 'other Content',
+        'date_posted': 'January 02, 2025'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html',posts=posts)
 
-@app.route("/contact")
-def contact():
-    return "<h1>Contact Page</h1>"
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 
 
